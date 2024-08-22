abdffb3ed0ba107254ef42bdf25d290b1c2e1d92
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2024-08-21 22:21:32 -0300

    Adding Google Analytics and Google Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:math_app/firebase_options.dart';
 import 'package:math_app/services/realm.service.dart';
 import 'package:math_app/utils/preferences.util.dart';
 import 'package:math_app/views/main.view.dart';
@@ -8,6 +11,12 @@ void main() async {
 
   await RealmService.initRealm();
   await PreferencesUtil.initSharedPreferences();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 
   runApp(const MainView());
 }
